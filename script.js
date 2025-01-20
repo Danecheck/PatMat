ab9cc46321e46ce21129be566c5495224e35c300
Danecheck <susaofficial69@gmail.com>
2025-01-20 15:26:31 +0100

    řešení

--- script.js
+++ script.js
@@ -5,14 +5,17 @@ const pocetMat = document.getElementById("pocetMat");
 
 // kód vykreslení obrázků
 const pat = "<img src='img/pat.png'>";
+const mat = "<img src='img/mat.png'>";
 
 let kodFigurek = "";
 let i=0;
+let patMat = false;
+
 while (i<10) {
   i++;
-  kodFigurek += "<div>" + pat + "</div>";
+  kodFigurek += "<div>" + (patMat ? pat + mat : mat + pat) + "</div>";
 }
+
 figurky.innerHTML = kodFigurek; // vloží kód řady obrázků, kterou požadujeme vykreslit
 pocetPat.innerText = i;         // vypíše počet vykreslených figurek Pata (všech deset)
-pocetMat.innerText = 0;         // vypíše počet vykreslenách figurek Mata (žádný)
-
+pocetMat.innerText = i;         // vypíše počet vykreslenách figurek Mata (10)
