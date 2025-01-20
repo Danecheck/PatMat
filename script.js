8792fb426bc0bfdc1407429fd3f79eea771fb05f
Danecheck <susaofficial69@gmail.com>
2025-01-20 15:38:46 +0100

    řešení v2

--- script.js
+++ script.js
@@ -10,12 +10,22 @@ const mat = "<img src='img/mat.png'>";
 let kodFigurek = "";
 let i=0;
 let patMat = false;
+let pPat=0 ; 
+let pMat=0;
 
-while (i<10) {
+while (i<Math.random()*20) {
   i++;
-  kodFigurek += "<div>" + (patMat ? pat + mat : mat + pat) + "</div>";
+  if (Math.random()<0.5){
+    kodFigurek += "<div>" + pat + "</div>";
+    pPat++;
+  }else {
+    kodFigurek += "<div>" + mat + "</div>";
+    pMat++;
+  }
+  //i++;
+  //kodFigurek += "<div>" + (patMat ? pat + mat : mat + pat) + "</div>";
 }
 
 figurky.innerHTML = kodFigurek; // vloží kód řady obrázků, kterou požadujeme vykreslit
-pocetPat.innerText = i;         // vypíše počet vykreslených figurek Pata (všech deset)
-pocetMat.innerText = i;         // vypíše počet vykreslenách figurek Mata (10)
+pocetPat.innerText = pPat;         // vypíše počet vykreslených figurek Pata (všech deset)
+pocetMat.innerText = pMat;         // vypíše počet vykreslenách figurek Mata (10)
